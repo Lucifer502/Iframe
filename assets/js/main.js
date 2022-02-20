f8701d1c4102fa09079c376b9f853479cf883017
Carlos <87106034+Lucifer502@users.noreply.github.com>
2022-02-19 21:41:46 -0600

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,9 +3,8 @@ function startPlayer() {
     let playerInstance = jwplayer("player_div")
     playerInstance.setup({
       "playlist": [
-        { 
-          "title": episode_title,
-          "description": video_config_media['metadata']['title'],
-          "image": video_config_media['thumbnail']['url'],
+        {
           "sources": sources,
-},]}})
+}, ]
+    })
+  }
