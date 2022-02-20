3f322f638bba703fff6e0a2060cc65b719d48f8a
Carlos <87106034+Lucifer502@users.noreply.github.com>
2022-02-19 21:56:54 -0600

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,15 @@
-function startPlayer() {
+  window.addEventListener("message", async e => {
 
-    let playerInstance = jwplayer("player_div")
-    playerInstance.setup({
-      "playlist": [
-        {
-          "sources": sources,
+    let sources = e.data.video_config_media;
+
+    function startPlayer() {
+
+      let playerInstance = jwplayer("player_div")
+      playerInstance.setup({
+        "playlist": [
+          {
+            "sources": sources,
 }, ]
-    })
-  }
+      })
+    }
+  })
