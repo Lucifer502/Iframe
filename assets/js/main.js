cc7d96e9e3f5f6e4b5d97f4c6a8f758ac7341551
Carlos <87106034+Lucifer502@users.noreply.github.com>
2022-02-22 02:10:01 -0600

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,10 @@
   window.addEventListener("message", async e => {
 
-    let sources = e.data.video_config_media;
-    console.log(sources);
+    let video_m3u8_array = e.data.video_config_media;
+    let sources = [];
 
+    for (let idx of [1, 0, 2, 3, 4])
+      sources.push({ file: video_m3u8_array[idx], label: r[idx] + (idx < 2 ? '<sup><sup>HD</sup></sup>' : '') });
     startPlayer();
 
     function startPlayer() {
@@ -11,7 +13,7 @@
       playerInstance.setup({
         "playlist": [
           {
-            "file": sources,
+            "sources": sources,
 }, ]
       })
 
