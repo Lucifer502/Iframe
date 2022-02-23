cf1d1da3bbf506945d8dfcf87844a3e06a43e5bc
Carlos <87106034+Lucifer502@users.noreply.github.com>
2022-02-22 21:18:56 -0600

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,8 +2,11 @@
     
     const r = { 0: '720p', 1: '1080p', 2: '480p', 3: '360p', 4: '240p' };
 
+    let streamrgx = /_,(\d+.mp4),(\d+.mp4),(\d+.mp4),(\d+.mp4),(\d+.mp4),.*?manifest.mpd/;
+    let streamrgx_three = /_,(\d+.mp4),(\d+.mp4),(\d+.mp4),.*?manifest.mpd/;
+
     let video_config_media = e.data.video_config_media;
-    let video_m3u8_array = video_config_media;
+    let video_m3u8_array = m3u8ListFromStream(video_config_media[0]);
     let sources = [];
 
     for (let idx of [1, 0, 2, 3, 4])
@@ -23,5 +26,16 @@
       jwplayer().on('ready', e => {
         document.body.querySelector(".loading_container").style.display = "none";
       })
+    }
+
+    function m3u8ListFromStream(url) {
+    const cleanUrl = url.replace('evs1', 'evs').replace(url.split("/")[2], "fy.v.vrv.co");
+    const res = [];
+    for (let i in r)
+      if (streamrgx_three.test(cleanUrl) && i <= 2) // por algum motivo alguns videos da CR tem apenas 3 resoluções
+        res.push(cleanUrl.replace(streamrgx_three, `_$${(parseInt(i)+1)}`))
+      else
+        res.push(cleanUrl.replace(streamrgx, `_$${(parseInt(i)+1)}`))
+    return res;
     }
   })
