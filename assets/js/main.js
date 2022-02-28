b7821bdea7e7ebadf15faca591b4bd502b4bba82
Carlos <87106034+Lucifer502@users.noreply.github.com>
2022-02-27 20:42:48 -0600

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,33 +2,16 @@
     
     const r = { 0: '720p', 1: '1080p', 2: '480p', 3: '360p', 4: '240p' };
 
-    let streamrgx = /_,(\d+.mp4),(\d+.mp4),(\d+.mp4),(\d+.mp4),(\d+.mp4),.*?manifest.mpd/;
-    let streamrgx_three = /_,(\d+.mp4),(\d+.mp4),(\d+.mp4),.*?manifest.mpd/;
+    let streamrgx = /_,(\d+.mp4),(\d+.mp4),(\d+.mp4),(\d+.mp4),(\d+.mp4),.*?master.m3u8/;
+    let streamrgx_three = /_,(\d+.mp4),(\d+.mp4),(\d+.mp4),.*?master.m3u8/;
 
-    let video_config_media = e.data.video_config_media;
-    let video_m3u8_array = m3u8ListFromStream(video_config_media);
+    let video_config_media = JSON.parse(e.data.video_config_media);
+    let video_mp4_array = mp4ListFromStream(video_config_media);
     let sources = [];
 
-    for (let idx of [1, 0, 2, 3, 4])
-      sources.push({ file: video_m3u8_array[idx], label: r[idx] + (idx < 2 ? '<sup><sup>HD</sup></sup>' : '') });
-    startPlayer();
+    console.log(video_config_media);
 
-    function startPlayer() {
-
-      let playerInstance = jwplayer("player_div")
-      playerInstance.setup({
-        "playlist": [
-          {
-            "sources": sources,
-}, ]
-      })
-
-      jwplayer().on('ready', e => {
-        document.body.querySelector(".loading_container").style.display = "none";
-      })
-    }
-
-    function m3u8ListFromStream(url) {
+    function mp4ListFromStream(url) {
     const cleanUrl = url.replace('evs1', 'evs').replace('/p/', '/').replace(url.split("/")[2], "fy.v.vrv.co");
     const res = [];
     for (let i in r)
