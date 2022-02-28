8f248b157ea3f66196e3a60fabb459d605168ef1
Carlos <87106034+Lucifer502@users.noreply.github.com>
2022-02-27 21:15:14 -0600

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,13 +6,20 @@
     let streamrgx = /_,(\d+.mp4),(\d+.mp4),(\d+.mp4),(\d+.mp4),(\d+.mp4),.*?master.m3u8/;
     let streamrgx_three = /_,(\d+.mp4),(\d+.mp4),(\d+.mp4),.*?master.m3u8/;
     let video_config_media = JSON.parse(e.data.video_config_media);
-    let allorigins = "https://crp-proxy.herokuapp.com/get?url=";
+    let video_stream_url = "";
     let video_mp4_array = [];
     let user_lang = e.data.lang;
     let rows_number = 0;
     let sources = [];
 
-    console.log(video_config_media);
+    console.log(video_mp4_array);
+
+    let dlSize = [];
+    let dlUrl = [];
+    for (let idx in r) {
+      dlSize[idx] = document.getElementById(r[idx] + "_down_size");
+      dlUrl[idx] = document.getElementById(r[idx] + "_down_url");
+    }
 
     const streamlist = video_config_media['streams'];
     for (let stream of streamlist) {
@@ -36,14 +43,52 @@
       // Padrão
       if (stream.format == 'adaptive_hls' && stream.hardsub_lang == user_lang) {
         video_stream_url = stream.url;
-        video_m3u8_array = await m3u8ListFromStream(video_stream_url);
         video_mp4_array = mp4ListFromStream(video_stream_url);
         break;
       }
     }
 
+    function linkDownload(id, tentativas = 0) {
+      console.log('  - Baixando: ', r[id])
+      let video_mp4_url = video_mp4_array[id];
+
+      let fileSize = "";
+      let http = (window.XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject("Microsoft.XMLHTTP"));
+      http.onreadystatechange = () => {
+        if (http.readyState == 4 && http.status == 200) {
+          fileSize = http.getResponseHeader('content-length');
+          if (!fileSize)
+            return setTimeout(() => linkDownload(id), 5000);
+          else {
+            let sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB'];
+            if (fileSize == 0) return console.log('addSource#fileSize == 0');
+            let i = parseInt(Math.floor(Math.log(fileSize) / Math.log(1024)));
+            if (i == 0) return console.log('addSource#i == 0');
+            let return_fileSize = (fileSize / Math.pow(1024, i)).toFixed(1) + ' ' + sizes[i];
+            dlSize[id].innerText = return_fileSize;
+            return console.log(`[CR Premium] Source adicionado: ${r[id]} (${return_fileSize})`);
+          }
+        } else if (http.readyState == 4 && tentativas < 3)
+          return setTimeout(() => linkDownload(id, tentativas + 1), 5000);
+      }
+      http.open("HEAD", video_mp4_url, true);
+      http.send(null);
+    }
+
+    console.log('[CR Premium] Baixando sources:')
+    for (let id of [1, 0, 2, 3, 4])
+      linkDownload(id);
+    // Definir URL e Tamanho na lista de download
+    for (let id of [1, 0, 2, 3, 4]) {
+      dlUrl[id].href = video_mp4_array[id];
+    }
+
+    function getDirectFile(url) {
+      return url.replace(/\/clipFrom.*?index.m3u8/, '').replace('_,', '_').replace(url.split("/")[2], "fy.v.vrv.co");
+    }
+
     function mp4ListFromStream(url) {
-      const cleanUrl = url.replace('evs1', 'evs').replace('/p/', '/').replace(url.split("/")[2], "fy.v.vrv.co");
+      const cleanUrl = url.replace('evs1', 'evs').replace(url.split("/")[2], "fy.v.vrv.co");
       const res = [];
       for (let i in r)
         if (streamrgx_three.test(cleanUrl) && i <= 2) // por algum motivo alguns videos da CR tem apenas 3 resoluções
