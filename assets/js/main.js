7615da40e99c5951096b726874802666ad4ba44e
Carlos <87106034+Lucifer502@users.noreply.github.com>
2022-03-01 18:59:01 -0600

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,6 @@
 window.addEventListener("message", async e => {
 
-  let video_config_media = e.data.stream;
+  let video_config_media = e.data.video_config_media;
 
   console.log(video_config_media);
 
