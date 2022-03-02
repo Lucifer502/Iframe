ec139696a28fc4856d74ebd4e0e2e1823b2a4957
Carlos <87106034+Lucifer502@users.noreply.github.com>
2022-03-01 19:47:33 -0600

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,7 +2,7 @@
 window.addEventListener("message", async e => {
 
   let initial_state = e.data.video_config_media;
-  let id = video_config_media.watch.id;
+  let id = initial_state.watch.id;
   let config_media = initial_state.content.byId[id]
   let playback = config_media.playback;
   console.log(playback)
