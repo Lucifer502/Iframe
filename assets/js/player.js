8b40ed5f81e0161b0924bd1b0c33e207fb72c55b
Carlos <87106034+Lucifer502@users.noreply.github.com>
2022-03-01 22:42:13 -0600

    Update player.js

--- assets/js/player.js
+++ assets/js/player.js
@@ -1,14 +1,53 @@
-
 window.addEventListener("message", async e => {
 
-  let initial_state = e.data.video_config_media;
-  let id = initial_state.watch.id;
-  let config_media = initial_state.content.byId[id]
-  let playback = config_media.playback;
-  console.log(playback)
+  const promises = [],
+    request = [];
+  const r = { 0: '720p', 1: '1080p', 2: '480p', 3: '360p', 4: '240p' };
 
+  let streamrgx = /_,(\d+.mp4),(\d+.mp4),(\d+.mp4),(\d+.mp4),(\d+.mp4),.*?master.m3u8/;
+  let streamrgx_three = /_,(\d+.mp4),(\d+.mp4),(\d+.mp4),.*?master.m3u8/;
+  let allorigins = "https://crp-proxy.herokuapp.com/get?url=";
+  let playback = e.data.playback;
+  let video_config_media = await getStreams(playback);
+  let video_mp4_array = [];
 
+  let user_lang = 'es-LA';
 
+  const streamlist = video_config_media['streams']
+  video_mp4_array = mp4ListFromStream(streamlist.adaptive_hls[user_lang].url);
+
+  function mp4ListFromStream(url) {
+    const cleanUrl = url.replace('evs1', 'evs').replace(url.split("/")[2], "fy.v.vrv.co");
+    const res = [];
+    for (let i in r)
+      if (streamrgx_three.test(cleanUrl) && i <= 2) // por algum motivo alguns videos da CR tem apenas 3 resoluções
+        res.push(cleanUrl.replace(streamrgx_three, `_$${(parseInt(i)+1)}`))
+    else
+      res.push(cleanUrl.replace(streamrgx, `_$${(parseInt(i)+1)}`))
+    return res;
+  }
+
+  console.log(video_mp4_array)
+
+  function getAllOrigins(url) {
+    return new Promise(async (resolve, reject) => {
+      await $.ajax({
+          async: true,
+          type: "GET",
+          url: allorigins + encodeURIComponent(url),
+          responseType: 'json'
+        })
+        .then(res => {
+          resolve(res.contents)
+        })
+        .catch(err => reject(err));
+    })
+  }
+
+  async function getStreams(url) {
+    const metadata = JSON.parse(await getAllOrigins(url));
+    return metadata;
+  }
 
 
 })
