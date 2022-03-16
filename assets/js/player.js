583a9b919844b25db4bd1490187e72bb3465fa23
Carlos <87106034+Lucifer502@users.noreply.github.com>
2022-03-16 10:27:41 -0600

    Update player.js

--- assets/js/player.js
+++ assets/js/player.js
@@ -7,18 +7,15 @@ window.addEventListener("message", async e => {
  let streamrgx = /_,(\d+.mp4),(\d+.mp4),(\d+.mp4),(\d+.mp4),(\d+.mp4),.*?master.m3u8/;
  let streamrgx_three = /_,(\d+.mp4),(\d+.mp4),(\d+.mp4),.*?master.m3u8/;
  let allorigins = "https://crp-proxy.herokuapp.com/get?url=";
- let playback = e.data.playback;
- let video_config_media = await getStreams(playback);
  let video_mp4_array = [];
  let sources = [];
- let thumbnail = e.data.thumbnail;
-
+ let video_config_media = e.data.array;
+ console.log(video_config_media);
  let user_lang = 'es-LA';
 
- const streamlist = video_config_media['streams']
- video_mp4_array = mp4ListFromStream(streamlist.adaptive_hls[user_lang].url);
+ 
 
- console.log(video_mp4_array)
+ 
 
  for (let idx of [1, 0, 2, 3, 4]) sources.push({ file: video_mp4_array[idx], label: r[idx] + (idx < 2 ? '<sup><sup>HD</sup></sup>' : '') });
  startPlayer();
@@ -28,7 +25,6 @@ window.addEventListener("message", async e => {
   let playerInstance = jwplayer('player_div')
   playerInstance.setup({
    'playlist': [{
-    'image': thumbnail,
     'sources': sources,
   }]
   })
@@ -39,16 +35,7 @@ window.addEventListener("message", async e => {
   });
  }
 
- function mp4ListFromStream(url) {
-  const cleanUrl = url.replace('evs1', 'evs').replace(url.split("/")[2], "fy.v.vrv.co");
-  const res = [];
-  for (let i in r)
-   if (streamrgx_three.test(cleanUrl) && i <= 2) // por algum motivo alguns videos da CR tem apenas 3 resoluções
-    res.push(cleanUrl.replace(streamrgx_three, `_$${(parseInt(i)+1)}`))
-  else
-   res.push(cleanUrl.replace(streamrgx, `_$${(parseInt(i)+1)}`))
-  return res;
- }
+ 
 
  function getAllOrigins(url) {
   return new Promise(async (resolve, reject) => {
