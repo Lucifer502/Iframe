ceb87bf1fd57fbd6e8a170a248a531785583837e
Carlos <87106034+Lucifer502@users.noreply.github.com>
2022-03-18 09:16:56 -0600

    Update player.js

--- assets/js/player.js
+++ assets/js/player.js
@@ -2,4 +2,30 @@ window.addEventListener("message", async e => {
 
  let playbacks = e.data.playbacks;
 console.log(playbacks);
+
+let video_config_media  = await getStreams(playbacks)
+
+console.log(video_config_media)
+
+function getAllOrigins(url) {
+    return new Promise(async (resolve, reject) => {
+      await $.ajax({
+        async: true,
+        type: "GET",
+        url: needproxy ? allorigins + encodeURIComponent(url) : url,
+        responseType: 'json'
+      })
+      .then(res=>{
+        resolve(res.contents ?? res)
+      })
+      .catch(err=>reject(err));
+    })
+  }
+
+async function getStreams(url) {
+const streams = await getAllOrigins(url);
+return streams
+}
+
+
 });
