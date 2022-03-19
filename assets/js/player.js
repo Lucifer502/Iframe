629a841b57c73c7a514441e009567deee369539e
Carlos <87106034+Lucifer502@users.noreply.github.com>
2022-03-19 09:22:59 -0600

    Update player.js

--- assets/js/player.js
+++ assets/js/player.js
@@ -40,57 +40,56 @@ window.addEventListener("message", async e => {
  for (let idx of [1, 0, 2, 3, 4])
   sources.push({ file: video_mp4_array[idx], label: r[idx] + (idx < 2 ? '<sup><sup>HD</sup></sup>' : '') });
  startPlayer();
-});
 
-function startPlayer() {
+ function startPlayer() {
 
- let playerInstance = jwplayer('player_div')
- playerInstance.setup({
-  'playlist': [{
-   'image': thumbnail,
-   'sources': sources,
+  let playerInstance = jwplayer('player_div')
+  playerInstance.setup({
+   'playlist': [{
+    'image': thumbnail,
+    'sources': sources,
   }]
- })
+  })
 
- jwplayer().on('ready', e => {
+  jwplayer().on('ready', e => {
 
-  document.body.querySelector(".loading_container").style.display = "none";
- });
-}
+   document.body.querySelector(".loading_container").style.display = "none";
+  });
+ }
 
-function getAllOrigins(url) {
- return new Promise(async (resolve, reject) => {
-  await $.ajax({
-    async: true,
-    type: "GET",
-    url: allorigins + encodeURIComponent(url),
-    responseType: 'json'
-   })
-   .then(res => {
-    resolve(res.contents ?? res)
-   })
-   .catch(err => reject(err));
- })
-}
+ function getAllOrigins(url) {
+  return new Promise(async (resolve, reject) => {
+   await $.ajax({
+     async: true,
+     type: "GET",
+     url: allorigins + encodeURIComponent(url),
+     responseType: 'json'
+    })
+    .then(res => {
+     resolve(res.contents ?? res)
+    })
+    .catch(err => reject(err));
+  })
+ }
 
-async function getHTML(url) {
- const html = await getAllOrigins(url)
- return html;
-}
+ async function getHTML(url) {
+  const html = await getAllOrigins(url)
+  return html;
+ }
 
-async function getStream(url) {
- const episodeStream = JSON.parse(await getAllOrigins(url));
- return episodeStream;
-}
+ async function getStream(url) {
+  const episodeStream = JSON.parse(await getAllOrigins(url));
+  return episodeStream;
+ }
 
-function mp4ListFromStream(url) {
- const cleanUrl = url.replace('evs1', 'evs').replace(url.split("/")[2], "fy.v.vrv.co");
- const res = [];
- for (let i in r)
-  if (streamrgx_three.test(cleanUrl) && i <= 2) // por algum motivo alguns videos da CR tem apenas 3 resoluções
-   res.push(cleanUrl.replace(streamrgx_three, `_$${(parseInt(i)+1)}`))
- else
-  res.push(cleanUrl.replace(streamrgx, `_$${(parseInt(i)+1)}`))
- return res;
-}
+ function mp4ListFromStream(url) {
+  const cleanUrl = url.replace('evs1', 'evs').replace(url.split("/")[2], "fy.v.vrv.co");
+  const res = [];
+  for (let i in r)
+   if (streamrgx_three.test(cleanUrl) && i <= 2) // por algum motivo alguns videos da CR tem apenas 3 resoluções
+    res.push(cleanUrl.replace(streamrgx_three, `_$${(parseInt(i)+1)}`))
+  else
+   res.push(cleanUrl.replace(streamrgx, `_$${(parseInt(i)+1)}`))
+  return res;
+ }
 });
