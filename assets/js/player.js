286bb231e3fc570ed9fa7396e6a9a0450aa27a34
Carlos <87106034+Lucifer502@users.noreply.github.com>
2022-03-19 23:13:55 -0600

    Update player.js

--- assets/js/player.js
+++ assets/js/player.js
@@ -9,6 +9,7 @@ window.addEventListener("message", async e => {
  }
 
  let r = { 0: '720p', 1: '1080p', 2: '480p', 3: '360p', 4: '240p' };
+ let rgx = /http.*$/gm;
  let streamrgx = /_,(\d+.mp4),(\d+.mp4),(\d+.mp4),(\d+.mp4),(\d+.mp4),.*?master.m3u8/;
  let streamrgx_three = /_,(\d+.mp4),(\d+.mp4),(\d+.mp4),.*?master.m3u8/;
  let allorigins = "https://crp-proxy.herokuapp.com/get?url=";
