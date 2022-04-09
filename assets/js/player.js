1b9fd28c641437bb58adff5fbc1296e7202ef7e9
Carlos <87106034+Lucifer502@users.noreply.github.com>
2022-04-08 18:09:56 -0600

    Update player.js

--- assets/js/player.js
+++ assets/js/player.js
@@ -101,7 +101,7 @@ function getAllOrigins(url) {
       await $.ajax({
         async: true,
         type: "GET",
-        url: needproxy ? allorigins + encodeURIComponent(url) : url,
+        url: allorigins + encodeURIComponent(url),
         responseType: 'json'
       })
       .then(res=>{
