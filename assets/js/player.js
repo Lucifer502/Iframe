1756c6536b2d8c79e80e6900642e456a8068eb8c
Carlos <87106034+Lucifer502@users.noreply.github.com>
2022-05-07 00:33:37 -0600

    Update player.js

--- assets/js/player.js
+++ assets/js/player.js
@@ -1,11 +1,38 @@
-window.addEventListener('message', m3u8listfromstream, mp4listfromstream)
+window.addEventListener('load', message)
+
+function message(e) {
+
+ const r = { 0: '1080p', 1: '720p' }
+
+ let video_config_media = JSON.parse(e.data.video_config_media);
+ let sources = [];
+
+ const streamlist = video_config_media['streams'];
+ for (let stream of streamlist) {
+  if (stream.format == 'adaptive_hls' && stream.hardsub_lang == 'esLA') {
+   m3u8listfromstream(stream.url);
+  }
+ }
+
+ function startplayer(video_m3u8) {
+
+  for (let idx in r) {
+   sources.push({ file: video_m3u8[idx], label: r[idx] + (idx > 2 ? '<sup>HD</sup>' : '') })
+  }
+
+  let playerInstance = jwplayer('player');
+  playerInstance.setup({
+   'sources': sources,
+  })
+ }
+
+ function m3u8listfromstream(url) {
+  console.log(url)
+
+ }
 
 
+ function mp4listfromstream(url) {
 
-function m3u8listfromstream(e) {
-console.log(e)
-}
-
-function mp4listfromstream(e) {
-
+ }
 }
