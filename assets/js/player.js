75e7d242ed068e241704d7651f9a0af7fcf97387
Carlos <87106034+Lucifer502@users.noreply.github.com>
2022-05-12 13:20:33 -0600

    Update player.js

--- assets/js/player.js
+++ assets/js/player.js
@@ -28,7 +28,7 @@ window.addEventListener('message', async e => {
  for (let idx of [1, 0, 2, 3, 4])
   sources.push({ file: video_m3u8_array[idx], label: r[idx] + (idx < 2 ? '<sup><sup>HD</sup></sup>' : '') });
 
- let player = jwplayer('player_div')
+ let player = jwplayer('player')
  player.setup({
   'playlist': [{
    'sources': sources,
