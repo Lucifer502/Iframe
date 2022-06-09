f048fffa626cdc9f981b6a21eab300881182a35a
Carlos <87106034+Lucifer502@users.noreply.github.com>
2022-06-09 11:35:29 -0600

    Update player.js

--- assets/js/player.js
+++ assets/js/player.js
@@ -2,22 +2,11 @@ window.addEventListener("message", async e => {
 
 let playback = e.data.video_config_media.playback;
 let allorigins = "https://crp-proxy.herokuapp.com/get?url=";
-const streamlist = await getAllOrigins(playback).then(res=>res.json())
+const streamlist = await getAllOrigins(playback)
 
 console.log(streamlist)
 
 function getAllOrigins(url) {
-    return new Promise(async (resolve, reject) => {
-      await $.ajax({
-          async: true,
-          type: "GET",
-          url: allorigins + encodeURIComponent(url),
-          responseType: 'json'
-        })
-        .then(res => {
-          resolve(res.contents ?? res)
-        })
-        .catch(err => reject(err));
-    })
-  }
+return fetch(url)
+    }
 })
