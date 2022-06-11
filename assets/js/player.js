d8d38266c396a833ac6339b918ba21b5b3a60ce2
Carlos <87106034+Lucifer502@users.noreply.github.com>
2022-06-10 18:26:03 -0600

    Update player.js

--- assets/js/player.js
+++ assets/js/player.js
@@ -3,24 +3,11 @@ window.addEventListener("message", async e => {
 
 let allorigins = "https://api.allorigins.win/get?url="
 
-console.log(e.data)
-const res = await getAllOrigins(e.data);
+let playback = e.data.playback
 
-var ifrm = document.createElement('iframe')
-  ifrm.setAttribute('style', 'display:none');
-  ifrm.setAttribute('frameborder', '0');
-  ifrm.setAttribute('scrolling', 'no');
-  ifrm.setAttribute('width', '0px');
-  ifrm.setAttribute('height', '0px');
-  ifrm.setAttribute('id', 'ifrm');
-  ifrm.setAttribute('src', 'window.top.location.href');
+const streamlist = getAllOrigins(playback)
 
-  document.body.appendChild(ifrm)
-  ifrm.onload = () => {
-    ifrm.contentWindow.postMessage(res, '*')
-  }
-
-console.log(res)
+console.log(streamlist)
 
 function getAllOrigins(url) {
     return new Promise(async (resolve, reject) => {
